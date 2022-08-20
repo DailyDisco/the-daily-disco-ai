83788aac9560639b381e52f9c0167f95c2300524
Diego (D) <actual.mensch0r@icloud.com>
2022-08-19 21:33:29 -0500

    update button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,12 @@
-
 const Button = ({ btnName, classStyles, handleClick }) => (
   <button
     type="button"
     className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
     onClick={handleClick}
+    // here is where you we give our button functionality
   >
     {btnName}
+    {/* using this we can change the btnName based off certain pages or criteria such as being signed in */}
   </button>
 );
 
