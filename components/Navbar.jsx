ba886953cfcb912a28d895871e3e21fc10752b05
Diego (D) <actual.mensch0r@icloud.com>
2022-08-19 22:49:04 -0500

    fix navbar add footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -37,7 +37,7 @@ const MenuItems = ({ isMobile, active, setActive }) => {
     </ul>
   );
 };
-
+// the button will show connect when you need to connect your wallet and create when you are signed in
 const ButtonGroup = ({ setActive, router }) => {
   const hasConnected = true;
 
@@ -58,23 +58,26 @@ const navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
           <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            {/* change logo here */}
             {/* <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" /> */}
             <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">DigitalMe</p>
           </div>
         </Link>
         <Link href="/">
           <div className="hidden md:flex" onClick={() => {}}>
-            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            {/* change logo here */}
+            {/* <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" /> */}
           </div>
         </Link>
       </div>
-      <div className="flex flex-inital flex-row justify-end">
+      <div className="flex flex-initial flex-row justify-end">
         <div className="flex items-center mr-2">
           <input
             type="checkbox"
@@ -89,12 +92,50 @@ const navbar = () => {
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
         </div>
-      </div>
-      <div className="md:hidden flex">
-        <MenuItems active={active} setActive={setActive} />
-        <div className="ml-4">
-          <ButtonGroup setActive={setActive} router={router} />
+
+        <div className="md:hidden flex">
+          <MenuItems active={active} setActive={setActive} />
+          <div className="ml-4">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
         </div>
+      </div>
+      {/* this is our mobile navigation bar */}
+      <div className="hidden md:flex ml-2">
+        {isOpen
+          ? (
+            <Image
+              src={images.cross}
+              objectFit="contain"
+              width={20}
+              height={20}
+              alt="close"
+              onClick={() => setIsOpen(false)}
+              // this next line is the toggle that we click to change it from light mode to dark mode
+              className={theme === 'light' && 'filter invert'}
+            />
+          ) : (
+            <Image
+              src={images.menu}
+              objectFit="contain"
+              width={25}
+              height={25}
+              alt="menu"
+              onClick={() => setIsOpen(true)}
+              // this next line is the toggle that we click to change it from light mode to dark mode
+              className={theme === 'light' && 'filter invert'}
+            />
+          )}
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems active={active} setActive={setActive} isMobile />
+          </div>
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+        )}
       </div>
     </nav>
   );
