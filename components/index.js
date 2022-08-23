c19e1fd4f431211bc4288beb00e77243ff9ead2a
Diego (D) <actual.mensch0r@icloud.com>
2022-08-23 00:05:37 -0500

    esllint

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-cycle
 import Navbar from './Navbar';
+// eslint-disable-next-line import/no-cycle
 import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
