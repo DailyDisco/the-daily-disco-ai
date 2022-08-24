5d58e025e3787fd252436e4060f54a8d35b225bd
Diego (D) <actual.mensch0r@icloud.com>
2022-08-23 20:54:20 -0500

    fix escape characters

--- pages/about.js
+++ pages/about.js
@@ -18,10 +18,10 @@ export default function about() {
             </p>
           </div>
           <div className="mt-5">
-            <p>We also host a Stable Diffusion gallery. If you'd like your prompt created feel free to email me. We are currently working on integrated user self-input for our site. Sign up for our newsletter if you'd like to know when that's available!</p>
+            <p>We also host a Stable Diffusion gallery. If you would like your prompt created feel free to email me. We are currently working on integrated user self-input for our site. Sign up for our newsletter to know when that is available!</p>
           </div>
           <div className="mt-5">
-            <p> Also, if you'd like to create an account with us using your email or your crypto wallet. All prompts you input will be stored and you can begin to comment on posts and reviews!
+            <p> Also, if you would like to create an account with us using your email or your crypto wallet. All prompts you input will be stored and you can begin to comment on posts and reviews!
             </p>
           </div>
         </div>
