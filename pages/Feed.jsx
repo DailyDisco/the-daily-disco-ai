7c4ce24591762d97d97b0295db0e06191ffc972c
Diego (D) <actual.mensch0r@icloud.com>
2022-11-24 00:40:35 -0600

    final layout update for vercel

--- pages/Feed.jsx
+++ pages/Feed.jsx
@@ -42,11 +42,13 @@ const Feed = () => {
   }
 
   return (
-    <div>
-      <h3 className="flex justify-center items-center text-2xl font-bold mb-7">
-        Home Feed
-      </h3>
-      <div>{pins && <MasonryLayout pins={pins} />}</div>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <h3 className="flex justify-center items-center text-2xl font-bold mb-7">
+          Home Feed
+        </h3>
+        <div>{pins && <MasonryLayout pins={pins} />}</div>
+      </div>
     </div>
   );
 };
